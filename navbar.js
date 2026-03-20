2df7b276414c9d26a55e7294a3aae1389324fae9
Wa130 <walter.acount@hotmail.com>
2026-03-20 14:18:35 +1100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,41 @@
+class NavBar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <nav class="navbar">
+        <div id="navcontainer">
+        <div id="navlistholder">
+        <ul>
+            <li><a id="nav_default" href="index.html">Home</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+        </div>
+        </div>
+        </nav>
+    `;
+  }
+}
+
+customElements.define("nav-bar", NavBar);
+
+
+
+function highlightActive() {
+    const ACTIVE_CLASS = "nav_active";
+
+	const current = window.location.pathname;
+
+	document.querySelectorAll("nav-bar ul li a").forEach(link => {
+		if (new URL(link.href).pathname === current) {
+		    link.id = ACTIVE_CLASS;
+		}
+	});
+
+    if (current == "/" || current == "") {
+        document.getElementById("nav_default").id = ACTIVE_CLASS;
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    highlightActive();
+});
